a392df50151f7ee12b003c025195a14a28f04304
Travis Willard <twillard2@gmail.com>
2019-04-04 14:18:42 -0400

    Add ssh-agent support for mac

--- bashrc.d/macos.sh
+++ bashrc.d/macos.sh
@@ -41,3 +41,26 @@ export ANDROID_NDK=/usr/local/Cellar/android-ndk/r7b
 
 # Make the title something useful before invoking screen
 alias screen="title \"$(hostname | cut -d. -f1)(screen)\"; screen"
+
+# ssh-agent
+SSH_ENV=$HOME/.ssh/environment
+
+# start the ssh-agent
+function start_agent {
+    echo "Initializing new SSH agent..."
+    # spawn ssh-agent
+    /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
+    echo succeeded
+    chmod 600 "${SSH_ENV}"
+    . "${SSH_ENV}" > /dev/null
+    /usr/bin/ssh-add
+}
+
+if [ -f "${SSH_ENV}" ]; then
+     . "${SSH_ENV}" > /dev/null
+     ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
+        start_agent;
+    }
+else
+    start_agent;
+fi
